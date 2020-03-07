8652454cc591d7abda1da45102a9dcd1771d546d
ohmaga <ohmaga@naver.com>
2020-03-07 15:29:17 +0900

    step2 management

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class Subject extends Component{
   render(){
     return(
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -19,7 +19,7 @@ class TOC extends Component{
         <ul>
             <li><a href="1.html">HTML</a></li>
             <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JSX</a></li>
+            <li><a href="3.html">JavaScript</a></li>
         </ul>
       </nav>
     );
@@ -29,8 +29,8 @@ class Content extends Component{
   render(){
     return(
       <article>
-        <h2>HTML</h2>
-        HTML is Hypertext Markup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article> 
     );
   }
@@ -39,9 +39,10 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <Subject/>
-        <Content/>
-        <TOC/>
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     )
   }
