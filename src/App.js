b0c9e782a37352f108efc5b6635c666e96d8e393
ohmaga <ohmaga@naver.com>
2020-03-07 16:25:23 +0900

    step4 management

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import Content from './components/Content';
 import Subject from './components/Subject'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub: 'World wide web'},
+      content:{title:'HTML', desc:'HTML is HyperText Markup Language'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render(){
     return(
       <div className="App">
-        <Subject title="WEB" sub="world wide web"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title="React" sub="For UI"></Subject> */}
+        <TOC data={this.state.contents}></TOC>
+        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
       </div>
     )
   }
