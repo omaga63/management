bd1969bd893d9955dc593113cd09683dfede7467
ohmaga <ohmaga@naver.com>
2020-03-07 14:42:40 +0900

    step1 management

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JSX</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML is Hypertext Markup Language.
+      </article> 
+    );
+  }
+}
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <Subject/>
+        <Content/>
+        <TOC/>
+      </div>
+    )
+  }
 }
 
 export default App;
