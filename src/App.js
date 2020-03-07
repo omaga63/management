746424e9fdc7d6cf2cfd66cf9b249c481ca7dcfa
ohmaga <ohmaga@naver.com>
2020-03-07 17:10:20 +0900

    step5 management

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'welcome',
+      welcome:{title:'welcome', desc:'Hello, React!!'},
       subject:{title:'WEB', sub: 'World wide web'},
-      content:{title:'HTML', desc:'HTML is HyperText Markup Language'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,12 +19,21 @@ class App extends Component {
     }
   }
   render(){
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode==='welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         {/* <Subject title="React" sub="For UI"></Subject> */}
         <TOC data={this.state.contents}></TOC>
-        <Content title={this.state.content.title} desc={this.state.content.desc}></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }
